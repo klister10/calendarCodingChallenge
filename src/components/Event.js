6af481c7f2c9a9aa2e165507f3a8a3f25a40dcf0
Kathleen Lister <kathleen@amiralearning.com>
2024-05-17 14:54:15 -0700

    cleanup and add comments

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Event.scss';
 
+// Event component to be layered on top of the calendar day
 const Event = ({ event, hourBlockHeight, style, onMouseOver, onMouseLeave, handleClick }) => {
   const eventStartHour = parseInt(event.startTime.split(':')[0], 10);
   const eventEndHour = parseInt(event.endTime.split(':')[0], 10);
   const eventDuration = eventEndHour - eventStartHour;
 
+  // prevent the parent block from receiving a click event when the event is clicked
+  // also, call the callback passed down from the parent
   const onClick = (event) => {
     event.stopPropagation();
     handleClick();
